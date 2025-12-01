4edccbddb5bd942060db854b12d975d93c9f6deb
CarlosAL <carlosalmendras66@gmail.com>
2025-12-01 10:34:30 +0100

    Update conexionBBDD.php

--- api/conexionBBDD.php
+++ api/conexionBBDD.php
@@ -1,10 +1,16 @@
 <?php
 
 $Repit=false;
-$host="localhost";
-$user="root";
-$password="fqfq2wgh3h3h";
 
-$link= mysqli_connect($host,$user,$password);
-$tildes=$link->query("SET NAMES 'utf8'");
-mysqli_select_db($link,'periodicos');
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$password = getenv("DB_PASSWORD");
+$db_name = getenv("DB_NAME") ?: 'periodicos';
+
+$link = mysqli_connect($host, $user, $password);
+if(mysqli_connect_error()){
+    error_log("Conexión a la Base de Datos ha fallado: " . mysqli_connect_error());
+} else {
+    $tildes = $link->query("SET NAMES 'utf8'");
+    mysqli_select_db($link, $db_name);
+}
